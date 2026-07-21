65416a4e93935a81eada8b87c31d0bb29e426b9b
retn0 <nbsp1221@gmail.com>
2026-07-22 00:11:45 +0900

    ✨ Expose __version__ via importlib.metadata

--- packages/xret-data/src/xret/data/__init__.py
+++ packages/xret-data/src/xret/data/__init__.py
@@ -2,10 +2,14 @@
 
 from __future__ import annotations
 
+from importlib.metadata import version as _version
+
 from xret.data.config import MarketDataConfig
 from xret.data.dataset import BarDataset
 from xret.data.market_data import MarketData
 from xret.data.models import PartialScanResult, SyncResult
+
+__version__ = _version("xret-data")
 
 __all__ = [
     "MarketData",
